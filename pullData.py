9f87bc8d428772f6e95ed647b8b5e81ceb3432bb
Fiona Shyne <shynef@union.edu>
2026-05-12 12:40:52 -0400

    test code

--- pullData.py
+++ pullData.py
@@ -91,6 +91,8 @@ def save_all_with_scen(keys, folder="userData"):
     index =0 
     for key in keys:
         j = get_user_json(key)
+        if (key == 'R_3DIGpKwtFeW5KpD'):
+            print(j)
         if (not j is None and len(j["scenarios"]) > 0):
             j = json.dumps(j)
             file = open("{}/user_{}.json".format(folder, index), "w")
@@ -101,4 +103,5 @@ def save_all_with_scen(keys, folder="userData"):
 if __name__ == "__main__":
     print(get_all_keys())
 
-    save_all_with_scen(['Fiona', 'Fiona2'])
+    save_all_with_scen(['R_6v2pmvkPAdGrBiQ', 'R_5mHXGK7R0VQ7p3b', 'R_3pom7Qx4QlrEFUH', 'R_6382EfsWUcWRLx6', 'R_11WKnKcdtWNny9', 'R_5nSnL3WvPtcdWmc ', 'R_7ydRIdF5ye0KDk4', 'R_6RjZaXnEpBpjreF', 'R_3htPwMYJEe9sFkX', 'R_7SjDX79OmQ7uU6G ', 'R_51XUdaO4NnPznwM ', 'R_7hCJIWfZzz7Mlqx', 'R_7oldBhiPIzkUjf3 ', 'R_5FwA7uihmXLLbMG', 'R_19ndBcoh42pxOzB', 'R_3SDqtHyZ92X8ziC', 'R_1LI0xgpD53suPUf', 'R_1HwAUPKL22yUN5K', 'R_7VgZgYm4LnYpOrL', 'R_7UW99pXoUykDsCB', 'R_12XndVGiJL4P0XU', 'R_3mvGX5hddU8tXyh ', 'R_1mOuuIAkZmrQTct', 'R_3qNGVQQA5LQLdec', 'R_7PworAihDLMkjPZ', 'R_3sgYejRaamkHhf3', 'R_7pJKEWs8CtnPIPP', 'R_7jTIngcPBne6PwA', 'R_5Z9hIK8j3bjCNVv ', 'R_8gnZFrjEUA8cF8B', 'R_4PZgNS7RmTSc4DO', 'R_124JaHc2TzuKH9P', 'R_3dZ1qUwiD3ccV0o ', 'R_7OBwMMGOXLVmQRR ', 'R_7OBwMMGOXLVmQRR', 'R_4ClmTGwUC7IoaQN', 'R_6cRqIhCZsWRWaaZ ', 'R_5w5I3kKaZ1TBDaP', 'R_3Bu4tREMKPx6iyd', 'R_5zShAJbxMf0BB9e', 'R_17i1WNRsNTqcaMF', 'R_6XaMOpRu7xxUKt5', 'R_3DIGpKwtFeW5KpD', 'R_91uZMGlGigujnRV']
+)
